7289ffceda74027901427fb00d06209d55d19474
Eduardo Gomes <90475006+EdwardGomes@users.noreply.github.com>
2022-08-25 16:34:47 +0000

    Q5 de revisitando lógica resolvido

---
+++ RevisitandoLogica/q5.c
@@ -0,0 +1,16 @@
+//5.Faça um programa que leia a idade de uma pessoa e informe de ela é de maior ou menor
+#include <stdio.h>
+gg
+int main()
+{
+    int idade;
+    
+    printf("Insira a sua idade: \n");
+    scanf("%d", &idade);
+    if (idade >=18) {
+    printf("Você é de maior!");
+    }
+    else 
+    printf("Você é de menor!");    
+    
+}
