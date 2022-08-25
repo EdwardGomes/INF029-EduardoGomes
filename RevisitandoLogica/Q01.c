09f31d5ea8d5188e3f677f9cbc618b98c5465e37
Eduardo Gomes <90475006+EdwardGomes@users.noreply.github.com>
2022-08-25 16:01:17 +0000

    Q1 de revisitando lógica resolvida

---
+++ RevisitandoLogica/Q01.c
@@ -0,0 +1,8 @@
+//1. Faça um programa que imprima o seu nome, sua matrícula, e o seu semestre de ingresso no curso. Cada informação em uma linha.
+#include <stdio.h>
+
+int main() {
+  printf ("Nome: Eduardo Reis Gomes.\n");
+  printf("Matrícula: 20212160057.\n");
+  printf("Semestre: 2021.2.");
+}
