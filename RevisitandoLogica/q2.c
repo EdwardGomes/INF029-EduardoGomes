ce467adf3fd01262d1ecbc52d028d741593ba405
Eduardo Gomes <90475006+EdwardGomes@users.noreply.github.com>
2022-08-25 16:24:27 +0000

    Q2 de revisitando lógica resolvida

---
+++ RevisitandoLogica/q2.c
@@ -0,0 +1,16 @@
+//2.Faça um programa que leia a idade de duas pessoas e imprima a soma das idades
+#include <stdio.h>
+
+int main()
+{
+    int idade1, idade2, soma;
+    
+    printf("Coloque a idade da primeira pessoa: \n");
+    scanf("%d", &idade1);
+    printf("Coloque a idade da segunda pessoa: \n");
+    scanf("%d", &idade2);
+    soma = idade1+idade2;
+    
+    printf("A soma das idades é: %d",soma);
+
+}
