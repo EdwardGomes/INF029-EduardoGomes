cc19d99a879cd19590871424dff249f022c5ccee
Eduardo Gomes <90475006+EdwardGomes@users.noreply.github.com>
2022-08-30 23:31:15 +0000

    q8 resolvida

---
+++ RevisitandoLogica/q8.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+8.Faça um programa que calcula o fatorial de um número
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int fatorial;
+    int resposta=1;
+    printf("Insura um numero: \n");
+    scanf("%d", &fatorial);
+    
+    for ( ; fatorial >= 1; --fatorial){
+        resposta *= fatorial;
+    }
+    printf("O fatorial deste numero eh: %d", resposta);
+    return 0;
+}
