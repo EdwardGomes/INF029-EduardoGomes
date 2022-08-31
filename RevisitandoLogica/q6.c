7c2b0b58e1e7068e2189b0ad54364fa92b63078b
Eduardo Gomes <90475006+EdwardGomes@users.noreply.github.com>
2022-08-31 00:14:19 +0000

    q6 resolvida

---
+++ RevisitandoLogica/q6.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+6.Faça um programa que leia a idade de uma pessoa e informe de ela é de maior ou de menor
+. O programa deve repetir essa ação até que uma idade negativa seja informada
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   int idade;
+   
+   printf("Insira sua idade: \n");
+   scanf("%d", &idade);
+   do {
+       printf("Voce eh de maior!\n");
+       printf("Insira sua idade: \n");
+       scanf("%d", &idade);
+   }
+   while (idade >=18);
+   
+   if (idade <=17) {
+       printf("voce eh de menor, fim do algoritmo");
+   }
+   
+}
