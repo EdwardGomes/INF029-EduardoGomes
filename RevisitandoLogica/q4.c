733eae2efcd9e6dd4ea3edb6a410b8096477121b
Eduardo Gomes <90475006+EdwardGomes@users.noreply.github.com>
2022-09-05 17:26:29 +0000

    q4 e q7

---
+++ RevisitandoLogica/q4.c
@@ -0,0 +1,18 @@
+/*Faça um programa que leia dois valores informados pelo usuário e inverta os valores nas variáveis lidas sem utilizar variável auxiliar*/
+#include <stdio.h>
+
+int main(){
+  int a, b;
+
+  printf("Digite o valor do primeiro número: ");
+  scanf("%d", &a);
+
+  printf("Digite o valor do segundo número: ");
+  scanf("%d", &b);
+
+  a = a+b;
+  b = a-b;
+  a = a-b;
+
+  printf("Os valores após inversão: %d e %d\n", a, b);
+}
